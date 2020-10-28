f8e8ab228ca76bd8f71826b04867a5d41c569b38
Manishankar Vakta <evilrider555@gmail.com>
2020-10-29 03:54:57 +0600

    Auth User interface
    Auth User interface update,
    Show hide info based on login status.

--- js/apps.js
+++ js/apps.js
@@ -2,10 +2,25 @@
 // Listen for auth status changes
 // ==================
 auth.onAuthStateChanged(user => {
+  const login = document.querySelectorAll('.login');
+  const logout = document.querySelector('.logout');
+  const title = document.querySelector('.title');
+  const user_email = document.querySelector('.user_email');
+
+  console.log(login);
   if(user){
     console.log('User Logged in: ', user.email);
+
+    login.forEach(item => item.style.display = "none");   
+    logout.style.display = "block";
+    title.innerHTML = "You are Logged in as";
+    user_email.innerHTML = user.email;
+
   }else{
-    console.log('User Logged Out');
+    login.forEach(item => item.style.display = "block");
+    logout.style.display = "none";
+    title.innerHTML = "Login with Firebase AUTH";
+    user_email.innerHTML = "";
   }
 });
 
