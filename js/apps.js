e77850dc6971b0c917cf4ba9ae527105fe180398
Manishankar Vakta <evilrider555@gmail.com>
2020-10-29 07:09:25 +0600

    Login with Facebook
    Login with Facebook done

--- js/apps.js
+++ js/apps.js
@@ -58,10 +58,9 @@ googleButton.addEventListener('click', (e) => {
   auth.signInWithPopup(provider).then(function(result) {
 
     // This gives you a Google Access Token. You can use it to access the Google API.
-    // var token = result.credential.accessToken;
-    // The signed-in user info.
     var user = result.user;
     console.log(user);
+    
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -78,4 +77,29 @@ googleButton.addEventListener('click', (e) => {
 
 // login with Facebook
 
+const facebookButton = document.querySelector('#facebookButton');
 
+facebookButton.addEventListener('click', (e) => {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  provider.addScope('user_birthday');
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+    console.log(user);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log(errorCode);
+  });
+
+});
